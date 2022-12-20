54806b4d1a69fc56302362cf3495413bfe90c124
takez0_o <takez0_o@protonmail.com>
2022-12-20 18:21:04 +0300

    updt

--- utils/client/client.go
+++ utils/client/client.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"os"
 
+	"github.com/RediSearch/redisearch-go/redisearch"
 	"github.com/go-redis/redis/v8"
+	redigo "github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
 )
 
@@ -23,5 +25,20 @@ func NewClient(id int) *redis.Client {
 		Password: password,
 		DB:       id,
 	})
+	return client
+}
+
+func NewSearchClient(index_name string) *redisearch.Client {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error:", err)
+		log.Fatal("Error loading .env file")
+	}
+	password := os.Getenv("REDIS_PASSWORD")
+	host := "localhost:6379"
+	pool := &redigo.Pool{Dial: func() (redigo.Conn, error) {
+		return redigo.Dial("tcp", host, redigo.DialPassword(password))
+	}}
+	client := redisearch.NewClientFromPool(pool, index_name)
 	return client
 }
