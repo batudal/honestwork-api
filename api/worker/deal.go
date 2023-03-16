81b91f0d8023cc96199acb11e1d89082eaf2813a
RedKidd <mertcmutlu@gmail.com>
2023-03-16 16:56:59 +0300

    .

--- api/worker/deal.go
+++ api/worker/deal.go
@@ -21,14 +21,11 @@ func NewDealWatcher() *DealWatcher {
 }
 
 func (r *DealWatcher) WatchDeals() {
-	deal_count := getDealCount()
-
-	fetchDeals(deal_count)
-	getJobs()
-	time.Sleep(time.Duration(1) * time.Minute)
+	fetchDeals()
+	time.Sleep(time.Duration(30) * time.Minute)
 }
 
-func fetchDeals(dealAmount int) {
+func fetchDeals() {
 	conf, err := config.ParseConfig()
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -46,70 +43,54 @@ func fetchDeals(dealAmount int) {
 		log.Fatal(err)
 	}
 
-	//loop through all deals
-	addr_map := make(map[string][]int64)
-	for i := 0; i <= dealAmount; i++ {
-		deal, err := instance.GetDeal(nil, big.NewInt(int64(i)))
-		if err != nil {
-			log.Fatal(err)
-		}
-		if deal.Recruiter.String() != "0x0000000000000000000000000000000000000000" {
-			addr_map[deal.Recruiter.String()] = append(addr_map[deal.Recruiter.String()], int64(i))
-			fmt.Println(addr_map)
-		}
+	deals, err := instance.GetAllDeals(nil)
+	for _, deal := range deals {
+		fmt.Println(deal)
+		writeJob(&deal)
 
 	}
+
 	defer client.Close()
+
 }
 
-func getDealCount() int {
+func writeJob(deal *hwescrow.HWEscrowDeal) {
 	conf, err := config.ParseConfig()
 	if err != nil {
 		fmt.Println("Error:", err)
 	}
 
-	client, err := ethclient.Dial(conf.Network.Arbitrum.RPCURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	escrow_address_hex := common.HexToAddress(conf.ContractAddresses.Escrow)
-
-	instance, err := hwescrow.NewHwescrow(escrow_address_hex, client)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	dealCount, err := instance.DealIds(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("deal count:", dealCount)
-
-	defer client.Close()
-	return int(dealCount.Int64())
-}
-
-func getJobs() {
-	//get all skills and loop
 	jobs_controller := controller.NewJobIndexer("jobsIndex")
-
 	jobs, err := jobs_controller.GetAllJobs()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error:", err)
 	}
-	fmt.Println("jobs:", jobs[0].DealId)
-	// jobs[0].DealId = "10"
-	// job_writer := controller.NewJobController(jobs[0].UserAddress, jobs[0].Slot)
-	// job_writer.SetJob(&jobs[0])
-	//set deal id for each job in database
+	for _, job := range jobs {
+		if job.DealId == -1 {
+			if job.UserAddress == addressToString(deal.Recruiter) {
+				if job.Slot == toInt(deal.JobId) {
+					job_writer := controller.NewJobController(job.UserAddress, job.Slot)
+					job.DealId = toInt(deal.JobId)
+					job.DealNetworkId = int(conf.Network.Arbitrum.ID)
+					job_writer.SetJob(&job)
+				}
+			}
+		}
+	}
 
 }
 
-// checks instead
+// function to convert integer to bigInt
+func toBigInt(num int) *big.Int {
+	return big.NewInt(int64(num))
+}
 
-// get network_id, recruiter_addr + job_id (job:recruiter_addr:job_id)
+// function to convert bigInt to integer
+func toInt(num *big.Int) int {
+	return int(num.Int64())
+}
 
-//JOB DEFINITION: map[recruiteraddress] = job id
-// check if job has already been updated
-// update job on database with the deal_network_id and deal_id
+// function to convert commonAddress to string
+func addressToString(address common.Address) string {
+	return address.Hex()
+}
