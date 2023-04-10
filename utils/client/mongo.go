2ccb679e013f875164d9a15675db9b2129396f62
takez0_o <takez0_o@protonmail.com>
2023-04-10 16:15:04 +0300

    .

--- utils/client/mongo.go
+++ utils/client/mongo.go
@@ -1,49 +1,28 @@
 package client
 
 import (
+	"context"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
-	"github.com/takez0o/honestwork-api/utils/config"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func NewMongoClient() *redis.Client {
+func NewMongoClient() *mongo.Client {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	conf, err := config.ParseConfig()
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
-		log.Fatal("Error loading config file")
+		panic(err)
 	}
 
-	password := os.Getenv("REDIS_PASSWORD")
-	host := os.Getenv("REDIS_HOST")
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     host,
-		Password: password,
-		DB:       conf.DB.ID,
-	})
-	return client
-}
-
-func NewMongoSearchClient(index_name string) *redisearch.Client {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	password := os.Getenv("REDIS_PASSWORD")
-	host := os.Getenv("REDIS_HOST")
-	pool := &redigo.Pool{Dial: func() (redigo.Conn, error) {
-		return redigo.Dial("tcp", host, redigo.DialPassword(password))
-	}}
-	client := redisearch.NewClientFromPool(pool, index_name)
 	return client
 }
